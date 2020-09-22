33fe107ff2ad4835c786364818a1eb937f59ec89
Subham Kumar shaw <44587185+shawji700@users.noreply.github.com>
2020-09-23 00:16:35 +0530

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,24 @@
+def print_rangoli(size):
+    n=97+size
+    a=""
+    for i in range(n-1,96,-1):
+        for j in range(n-1,i-1,-1):
+            a=a+chr(j)
+            if j>i:
+                a=a+"-"
+        for k in range(j+1,n):
+            a=a+"-"+chr(k)
+        print(a.center((size+(size-1)*3),"-"))
+        a=""   
+    for i in range(98,97+size):
+        for j in range(n-1,i-1,-1):
+            a=a+chr(j)
+            if j>i:
+                a=a+"-"
+        for k in range(j+1,n):
+            a=a+"-"+chr(k)
+        print(a.center((size+(size-1)*3),"-"))
+        a=""
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
